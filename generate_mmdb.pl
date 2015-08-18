f9392eaa9c629bfd1b3286098e8bfd49e772cffe
klady <klady@83f8c0fa-5cd7-49b8-a9ad-5e47a2db8e72>
2015-08-18 13:42:36 +0000

    Enabled more mmdb functionality re: csv columns
    Squashed commit of the following:
    
    commit 4a04e3842a203e52a655a24ed12351414d4a31a5
    Author: Kyle Lady <klady@duosecurity.com>
    Date:   Tue Aug 18 09:39:52 2015 -0400
    
        [neustardb] genericize script for moar flexibility
    
    commit 673c044504559db5dd1e1ed63a3faf4ebd43d319
    Author: Kyle Lady <klady@duosecurity.com>
    Date:   Tue Aug 18 09:12:18 2015 -0400
    
        [neustardb] flesh out data types for all columns
    
    git-svn-id: svn+ssh://svn.duo/svn/labs@28333 83f8c0fa-5cd7-49b8-a9ad-5e47a2db8e72

--- generate_mmdb.pl
+++ generate_mmdb.pl
@@ -8,6 +8,7 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use Getopt::Long qw(GetOptions);
 use List::Util;
 use MaxMind::DB::Writer::Tree;
 use Net::Works::Network;
@@ -19,31 +20,8 @@ use Text::CSV;
 # MMDB format docs: https://maxmind.github.io/MaxMind-DB/
 # MaxMind::DB:Writer::Tree API docs: https://metacpan.org/pod/MaxMind::DB::Writer::Tree
 
-my $num_args = $#ARGV + 1;
-if ($num_args > 1) {
-    die "Usage: generate_mmdb.pl <tor or neustar>\n";
-}
-
 # change input record separator from default of LF to CRLF
 $/ = "\r\n";
-
-my $db_type = $ARGV[0] // "";
-my $db_name = "";
-my $db_description = "";
-if ($db_type eq "tor") {
-    $db_name = "Tor-Project-List";
-    $db_description = "Tor project exit address listing";
-} elsif ($db_type eq "neustar" || $db_type eq "") {
-    $db_name = "Neustar-IP-Gold";
-    $db_description = "Neustar IP Intelligence Gold Edition";
-} else {
-    die "Usage: generate_mmdb.pl <tor or neustar>\n";
-}
-
-my %types = (
-    proxy_type => 'utf8_string',
-    proxy_level => 'utf8_string',
-);
 
 # record_size: there's no actual documentation as to how
 # to choose a value in any of the documentation online.
@@ -51,9 +29,72 @@ my %types = (
 # the arity of each node. I chose 24 because the example
 # in the docs used it, and I can't justify changing it.
 # In retrospect, "chose" is a bit strong. -klady
+my $record_size = 24;
+my $db_type = "neustar";
+my $ip_version = 4;
+
+GetOptions(
+	'db-type=s' => \$db_type,
+	'ipv=i' => \$ip_version,
+	'record-size=i' => \$record_size,
+) or die "Usage: $0 --db-type TYPE --ipv VERSION --record-size SIZE\n";
+
+my $db_name = "";
+my $db_description = "";
+if ($db_type eq "tor") {
+    $db_name = "Tor-Project-List";
+    $db_description = "Tor project exit address listing";
+} elsif ($db_type eq "neustar") {
+    $db_name = "Neustar-IP-Gold";
+    $db_description = "Neustar IP Intelligence Gold Edition";
+} else {
+    die "Invalid db type\n";
+}
+
+my %types = (
+    continent => 'utf8_string',
+    country => 'utf8_string',
+    country_code => 'utf8_string',
+    country_cf => 'uint16',
+    region => 'utf8_string',
+    state => 'utf8_string',
+    state_code => 'utf8_string',
+    state_cf => 'uint16',
+    city => 'utf8_string',
+    city_cf => 'uint16',
+    postal_code => 'utf8_string',
+    area_code => 'utf8_string',
+    time_zone => 'float',
+    latitude => 'float',
+    longitude => 'float',
+    dma => 'uint32',
+    msa => 'uint32',
+    connection_type => 'utf8_string',
+    line_speed => 'utf8_string',
+    ip_routing_type => 'utf8_string',
+    asn => 'uint32',
+    sld => 'utf8_string',
+    tld => 'utf8_string',
+    organization => 'utf8_string',
+    carrier => 'utf8_string',
+    anonymizer_status => 'utf8_string',
+    home => 'utf8_string',
+    organization_type => 'utf8_string',
+    naics_code => 'utf8_string',
+    isic_code => 'utf8_string',
+    geonames_id => 'uint32',
+    state_ref_id => 'uint32',
+    region_ref_id => 'uint32',
+    city_ref_id => 'uint32',
+    proxy_type => 'utf8_string',
+    proxy_level => 'utf8_string',
+    proxy_last_detected => 'utf8_string',
+    hosting_facility => 'boolean',
+);
+
 my $tree = MaxMind::DB::Writer::Tree->new(
-    ip_version            => 4,
-    record_size           => 24,
+    ip_version            => $ip_version,
+    record_size           => $record_size,
     database_type         => $db_name,
     languages             => ['en'],
     description           => { en => $db_description },
